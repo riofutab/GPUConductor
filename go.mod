1b10d8d9f8d527938c19b156171025a0bf2621a4
carlo <carlo@paeony.org>
2025-09-29 17:48:07 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module GPUConductor
+
+go 1.23
---
+++ main.go
@@ -0,0 +1,29 @@
+/**
+ * @Author:
+ * @Date: 2025-09-29 17:42:06
+ * @LastEditors:
+ * @LastEditTime: 2025-09-29 17:42:06
+ * @FilePath: main.go
+ * @Description: 这是默认设置,可以在设置》工具》File Description中进行配置
+ */
+package main
+
+import (
+	"fmt"
+)
+
+//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
+// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+
+func main() {
+	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
+	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
+	s := "gopher"
+	fmt.Printf("Hello and welcome, %s!\n", s)
+
+	for i := 1; i <= 5; i++ {
+		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
+		fmt.Println("i =", 100/i)
+	}
+}
